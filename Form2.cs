3787d8cf4c8850a39855dbbdf26deb93b6f9b3ac
umutkhrmn405714076 <101084367+umutkhrmn405714076@users.noreply.github.com>
2022-03-14 11:50:26 +0300

    Boyut Ayarlama
    yeni proje

---
+++ Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Form_A
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Enter Tuşu Çalışıyor");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Esc Tuşu Çalışıyor");
+        }
+
+        private void btnControlBox_Click(object sender, EventArgs e)
+        {
+            if (this.ControlBox == true)
+                this.ControlBox = false;
+            else
+                this.ControlBox = true;
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            FormB formB = new FormB();
+            formB.Show();
+        }
+
+        private void btnCenterToScreen_Click(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+        }
+
+        private void btnShowDialog_Click(object sender, EventArgs e)
+        {
+            FormB formB = new FormB();
+            formB.ShowDialog();
+        }
+    }
+}

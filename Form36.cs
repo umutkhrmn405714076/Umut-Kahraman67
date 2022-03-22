e70b93f0b8ebb2b36a44297c260b5ae4c57f88f9
umutkhrmn405714076 <101084367+umutkhrmn405714076@users.noreply.github.com>
2022-03-22 10:12:04 +0300

    VERİ DOĞRULAMA

---
+++ Form36.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Veri_doğrulama_uygulaması_form_tasarımı_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtAd.Text))
+            {
+                MessageBox.Show("KULLANICI ADINI GİRİNİZ.", "UYARI");
+            }
+            if (string.IsNullOrEmpty(txtSifre.Text))
+            {
+                MessageBox.Show("ŞİFREYİ GİRİNİZ", "UYARI");
+            }
+        }
+    }
+}

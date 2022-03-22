e668df1b15ba93499320726efc9da0f864fcbe6d
umutkhrmn405714076 <101084367+umutkhrmn405714076@users.noreply.github.com>
2022-03-22 08:47:43 +0300

    ToolTip uygulaması form tasarımı-1

---
+++ Form3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ToolTip_uygulaması_form_tasarımı_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ToolTip tt = new ToolTip();
+            tt.SetToolTip(txtAd,"ADINIZI GİRİNİZ.");
+            tt.SetToolTip(txtSoyad,"SOYADINIZI GİRİNİZ.");
+            tt.SetToolTip(txtDoğumTarihi,"DOĞUM TARİHİNİZİ GG/AA/YYYY ŞEKLİNDE GİRİNİZ.");
+            tt.SetToolTip(txtAdres,"ADRESİNİZİ GİRİNİZ.");
+            tt.SetToolTip(btnKaydet,"KAYDETMEK İÇİN TIKALYINIZ.");
+        }
+    }
+}

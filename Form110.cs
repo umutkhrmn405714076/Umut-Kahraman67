4b9a63f93fa5417778e17d6dffbb5f6d6e113eaa
umutkhrmn405714076 <101084367+umutkhrmn405714076@users.noreply.github.com>
2022-03-22 11:56:31 +0300

    Kompleks veri bağlama uygulaması form tasarımı-1

---
+++ Form110.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kompleks_veri_bağlama_uygulaması_form_tasarımı_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVeriBağla_Click(object sender, EventArgs e)
+        {
+            string[] diller = {"C#", "Java", "Python", "Delphi"};
+            listeVeriler.DataSource = diller;
+        }
+    }
+}

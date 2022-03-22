4c095f4c785e6f5ba2e590935430b0719b090428
umutkhrmn405714076 <101084367+umutkhrmn405714076@users.noreply.github.com>
2022-03-22 10:34:31 +0300

    Giriş Maskeleme

---
+++ Form 37.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Giriş_maskeleme_uygulaması_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form7_Load(object sender, EventArgs e)
+        {
+            maskTC.Mask = "00000000000";
+            maskTelefon.Mask = "(999)000 00 00";
+            maskDoğumTarihi.Mask = "00/00/0000";
+            maskKartNumarası.Mask = "0000-0000-0000-0000";
+            maskIP.Mask = "###-###-###-###";
+        }
+    }
+}

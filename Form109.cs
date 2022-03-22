38243c71464e8d558fc8f65d8548720df80d084e
umutkhrmn405714076 <101084367+umutkhrmn405714076@users.noreply.github.com>
2022-03-22 11:47:20 +0300

    Kompleks veri bağlama uygulaması form tasarımı-3

---
+++ Form109.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kompleks_veri_bağlama_uygulaması_form_tasarımı_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Sehirler>listSehirler = new List<Sehirler>(); 
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Sehirler sehir = new Sehirler();
+            sehir.Plaka = txtPlaka.Text;
+            sehir.SehirAdi =txtŞehirAdi.Text
+        }
+    }
+}

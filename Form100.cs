56963aea7b796852f165db033d5e9c45dd5b9bd5
umutkhrmn405714076 <101084367+umutkhrmn405714076@users.noreply.github.com>
2022-03-22 11:15:11 +0300

    Basit veri bağlama uygulaması-2

---
+++ Form100.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Basit_veri_bağlama_uygulaması_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBağla_Click(object sender, EventArgs e)
+        {
+            Ogrenciler ogrenci = new Ogrenciler();
+            ogrenci.Numara = 1111;
+            ogrenci.AdSoyad = "UMUT";
+            ogrenci.Alan = "BİLİŞİM TEKNOLOJİLERİ";
+            txtNumara.DataBindings.Add("Text",ogrenci, "Numara");
+            txtAdSoyad.DataBindings.Add("Text", ogrenci, "Ad Soyad");
+            txtAlan.DataBindings.Add("Text", ogrenci, "Alan");
+        }
+    }
+}
